0d588c48f00d90ae6bbd9b29a2a04d73aca1a823
AdamK <adamkoziel@outlook.com>
2020-06-15 19:04:53 +0100

    answers

---
+++ whodunit_answers.js
@@ -0,0 +1,231 @@
+// #### Episode 1
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Murderer is Miss Scarlet
+
+///////////////////////////////////////////////////////////////////////
+
+// #### Episode 2
+
+
+// const murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// This will give an error on line 26, reassigning to const
+// Professor Plum is a murderer
+
+///////////////////////////////////////////////////////////////////////
+
+// #### Episode 3
+
+
+// let murderer = 'Professor Plum';
+
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// First verdict: 'Mrs. Peacock' - let allows reassigning
+// Second verdict: 'Profesor Plum' - second verdict does not have an access to 'murderer' inside declareMurder, it grabs murderer outside of it
+
+///////////////////////////////////////////////////////////////////////
+
+// #### Episode 4
+
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+// Mrs. Peacock is suspect three, as 'Colonel Mustard' is only available inside the function
+
+///////////////////////////////////////////////////////////////////////
+
+//#### Episode 5
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// The weapon is the revolver
+
+///////////////////////////////////////////////////////////////////////
+
+//#### Episode 6
+
+
+// let murderer = 'Colonel Mustard';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Mrs. White is a murderer as it is last reassignment to global variable
+
+///////////////////////////////////////////////////////////////////////
+
+//#### Episode 7
+
+
+// let murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+
+//     unexpectedOutcome();
+//   }
+
+//   plotTwist();
+  
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Miss Scarlet is a murderer being a global variable - got this one wrong should be Mr. Green
+
+///////////////////////////////////////////////////////////////////////
+
+//#### Episode 8
+
+
+// const scenario = {
+//   murderer: 'Mrs. Peacock',
+//   room: 'Conservatory',
+//   weapon: 'Lead Pipe'
+// };
+
+// const changeScenario = function() {
+//   scenario.murderer = 'Mrs. Peacock';
+//   scenario.room = 'Dining Room';
+
+//   const plotTwist = function(room) {
+//     if (scenario.room === room) {
+//       scenario.murderer = 'Colonel Mustard';
+//     }
+
+//     const unexpectedOutcome = function(murderer) {
+//       if (scenario.murderer === murderer) {
+//         scenario.weapon = 'Candle Stick';
+//       }
+//     }
+
+//     unexpectedOutcome('Colonel Mustard');
+//   }
+
+//   plotTwist('Dining Room');
+// }
+// changeScenario()
+// console.log(scenario.weapon)
+
+// const declareWeapon = function() {
+//   return `The weapon is ${scenario.weapon}.`
+// }
+
+// changeScenario();
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// The candle stick is the weapon although got this one wrong at first as well.
+
+///////////////////////////////////////////////////////////////////////
+
+//#### Episode 9
+
+
+// let murderer = 'Professor Plum';
+
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// The murderer is Profesor Plum
+
+///////////////////////////////////////////////////////////////////////
